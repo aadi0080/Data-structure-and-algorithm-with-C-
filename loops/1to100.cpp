5b7a6cd48c6c5e1c9217d85fa4a99f351419b629
aadi0080 <aadarshjaiswal9696@gmail.com>
2026-07-12 22:08:46 +0530

    loops print1 to 100

---
+++ loops/1to100.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int  main (){
+    for(int i=1; i<=100;i++){
+        cout<<i<<endl;                    //forworrd counting 
+    }
+    for(int i =100; i>=1; i--){
+         cout<<i<<endl;                   // reverce counting
+    }
+}
