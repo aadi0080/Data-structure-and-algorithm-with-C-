1ad0fde7761940cd82d246c1263142f52371d7a8
aadi0080 <aadarshjaiswal9696@gmail.com>
2026-07-14 11:33:24 +0530

    AP ...

---
+++ loops/AP.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std; 
+int main (){
+    // 1 3 5 7 ........... >n time .
+   int n;
+    cout<<"enter strength :  ";
+    cin>>n;
+    int i=1;
+    for(i = 1;i <= 2*n-1;i=i+2 )          // >>>>> by AP formula 
+    cout<<i<<endl;
+
+
+
+
+
+//by locic 
+int a;
+a=1;
+for(int i=1;i<=n;i++){                //>>>>>> by logic saperate variable ..........
+cout<<a<<endl;
+a=a+2;
+}
+
+
+
+}
