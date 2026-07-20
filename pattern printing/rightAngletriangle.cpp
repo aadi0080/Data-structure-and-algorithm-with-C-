231c5d687937be6a09e1ad81c7bbccc07d66e873
aadi0080 <aadarshjaiswal9696@gmail.com>
2026-07-20 23:35:14 +0530

    pattern printing of triangle☠️☠️☠️

---
+++ pattern printing/rightAngletriangle.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int h ;
+    cout << "enter  height";
+    cin >>h;
+ for(int i=1; i<=h; i++){
+    for(int j=1; j<=i ; j++){
+        cout << " * ";
+    }
+    cout<<endl;
+ }   
+}
