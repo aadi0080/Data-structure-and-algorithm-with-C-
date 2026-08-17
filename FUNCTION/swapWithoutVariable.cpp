a425e721f602e084d95d55e92bebc3089e34d180
aadi0080 <aadarshjaiswal9696@gmail.com>
2026-08-17 21:20:25 +0530

    swap without extra  variable

---
+++ FUNCTION/swapWithoutVariable.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std; 
+int main (){
+    int a ; // a=10
+    int b;  // b=3
+    cout<<"enter  the both num : ";
+    cin>>a ;
+    cin>>b ; 
+    a=a+b; // a=13
+    b=a-b; // b=13-3 =10 
+    a=a-b; 
+    cout<<a<<" "<<b; 
+    
+
+}
