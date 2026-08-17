fb420bc7f2ac494b9180ac6bf8e5fb7632676226
aadi0080 <aadarshjaiswal9696@gmail.com>
2026-08-17 21:07:11 +0530

    entering inpointers solving sqap

---
+++ FUNCTION/addressOfArguement.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace  std ;
+int main (){
+    // int x; 
+    // int y ;
+    // cout<<&x<<"  is main  address of x";
+    // if x have decleared any value so the  address is diffr form actual value  
+    // example 
+    // int x= 2 ; 
+    // cout<<&x;<," is address of x=3 ";
+}
+
+
+// understanding is whenever  you passes value  of onne  function to
+//another function our  neural network forward  address of  that  
+// particular  thing ....
+ 
---
+++ FUNCTION/swapTwoNum.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std ;
+int main (){
+    int a , b ; 
+    cout<<"enter  the  both num. ";
+    cin>>a>>b;
+    int temp = a ; 
+    a=b; 
+    b=temp; 
+    cout<<a<<" "<<b ;
+
+}
