68ce246a4c45a2244cb13d2707bae35aee7560e7
aadi0080 <aadarshjaiswal9696@gmail.com>
2026-08-30 23:12:37 +0530

    finding  x in  the  array  :

---
+++ arrray 1/FindX.cpp
@@ -0,0 +1,27 @@
+//  find  x  in array  and input  x and  array  
+#include<iostream>
+using namespace  std  ; 
+int  main() {
+    // array of 5  
+    int  arr[5];
+    cout<<"enter  array:  ";
+    for(int i=0 ; i <=4  ; i++ ){
+        cin>>arr[i];
+    }
+    int x; 
+    cout<<"enter  the  no you want  ot  find ";
+    cin>>x; 
+    for(int i=0 ; i<=4 ; i++){
+        if(arr[i]==x){
+            cout<<arr[i]<<" "<<"at  index  of "<< i;
+        }
+        else {
+            cout<<"element not  found";
+            break;
+        }
+        
+    }
+    
+    
+
+}
